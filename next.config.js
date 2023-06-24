985f991e18484a5b5d04bda023e2c6905fdf254a
Edu <elprogramadorgt8@gmail.com>
2023-06-23 21:54:14 -0400

    ++ README in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+//THE MDX IS NOT WORKING PLEASE HELP XDDDD
+
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+};
+
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+        providerImportSource: '@mdx-js/react',
+    },
+});
+
+// module.exports = nextConfig
+
+module.exports = withMDX({
+    ...nextConfig,
+    // pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+});
