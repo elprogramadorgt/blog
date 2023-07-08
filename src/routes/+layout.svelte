eeffa20675a23fe695ce48ae5bb1886d475ce78e
Edu <elprogramadorgt8@gmail.com>
2023-07-07 22:09:48 -0500

    ++ initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Header from './Header.svelte';
+
+	import Footer from './Footer.svelte';
+	import 'open-props/style';
+	import 'open-props/normalize';
+	import 'open-props/buttons';
+
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style>
+	.layout {
+		height: 100%;
+		max-inline-size: 1440px;
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+		margin-inline: auto;
+		padding-inline: var(--size-7);
+	}
+
+	main {
+		padding-block: var(--size-9);
+	}
+
+	@media (min-width: 1440px) {
+		.layout {
+			padding-inline: 0;
+		}
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,57 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+	import * as config from '$lib/config';
+</script>
+
+<header>
+	<!-- Title -->
+	<a href="/" class="title">
+		<b>{config.title}</b>
+	</a>
+
+	<!-- Navigation -->
+	<ul class="links">
+		<li>
+			<a href="/about">About</a>
+		</li>
+		<li>
+			<a href="/contact">Contact</a>
+		</li>
+		<li>
+			<a href="/rss.xml" target="_blank">RSS</a>
+		</li>
+	</ul>
+
+	<!-- Theme -->
+	<button>Toggle</button>
+</header>
+
+<style>
+	nav {
+		padding-block: var(--size-7);
+	}
+
+	.links {
+		margin-block: var(--size-7);
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+	@media (min-width: 768px) {
+		nav {
+			display: flex;
+			justify-content: space-between;
+		}
+
+		.links {
+			display: flex;
+			gap: var(--size-7);
+			margin-block: 0;
+		}
+	}
+</style>
