0bc5cf1e1319a9ff57c6b04e3dd17902daa723b1
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-09-10 09:46:10 -0500

    ++ new URLS

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import * as config from '$lib/config';
+	import * as config from "$lib/config";
 </script>
 
 <nav>
@@ -11,13 +11,15 @@
 	<!-- Navigation -->
 	<ul class="links">
 		<li>
+			<a href="/blog">Blog</a>
+		</li>
+
+		<li>
+			<a href="/cursos">Cursos</a>
+		</li>
+
+		<li>
 			<a href="/about">About</a>
-		</li>
-		<li>
-			<a href="/contact">Contact</a>
-		</li>
-		<li>
-			<a href="/rss.xml" target="_blank">RSS</a>
 		</li>
 	</ul>
 
