917e0e2fdab5efc14c1bbb70c6df8fff5820d8bf
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-09-10 09:47:24 -0500

    ++ display post on blog tab

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import { formatDate } from "$lib/utils.js";
+
+    export let data;
+</script>
+
+<section class="pb-10">
+    <ul class="posts">
+        {#each data.posts as post}
+            <li class="post">
+                <a href={post.slug} class="title">{post.title}</a>
+                <p class="date">{formatDate(post.date)}</p>
+                <p class="description">{post.description}</p>
+            </li>
+        {/each}
+    </ul>
+</section>
+
+<style>
+    .posts {
+        display: grid;
+        gap: 2rem;
+    }
+
+    .post {
+        max-inline-size: var(--size-content-3);
+    }
+
+    .post:not(:last-child) {
+        border-bottom: 1px solid var(--border);
+        padding-bottom: var(--size-7);
+    }
+
+    .title {
+        font-size: var(--font-size-fluid-3);
+        text-transform: capitalize;
+    }
+
+    .date {
+        color: var(--text-2);
+    }
+
+    .description {
+        margin-top: var(--size-3);
+    }
+</style>
