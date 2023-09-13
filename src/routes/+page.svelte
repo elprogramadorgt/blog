4efedca01f5f042d2ebd08f8794757fa2a61e5d9
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-09-12 23:38:14 -0500

    ++ format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { formatDate } from '$lib/utils';
-	import * as config from '$lib/config';
+	import { formatDate } from "$lib/utils";
+	import * as config from "$lib/config";
 
 	export let data;
 </script>
