52c3c4aef71c8986bb5a9a867937d5ce38815b29
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-09-12 23:38:40 -0500

    ++ container to the main application

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,9 +9,8 @@
 	import "./app.css";
 </script>
 
-<div class="layout">
+<div class="container mx-auto">
 	<!-- Header -->
-	<p class="mb-3 text-gray-500 dark:text-gray-400 block">Hola a todos, como bien saben nuestra apliación de EduQuest esta hecha en Flutter y la autenticación de usuarios es con una cuenta de Gmail.</p>
 	<Header />
 
 	<!-- Black hole for other content -->
@@ -22,17 +21,7 @@
 </div>
 
 <style>
-	.layout {
-		height: 100%;
-		max-inline-size: 1440px;
-		display: grid;
-		grid-template-rows: auto 1fr auto;
-		margin-inline: auto;
-		padding-inline: var(--size-7);
-	}
-
 	main {
 		padding-block: var(--size-9);
 	}
-
 </style>
