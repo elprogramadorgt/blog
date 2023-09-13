09cc95a1896a4cf3c1a2819ab1e56430b80f8253
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-09-12 23:37:47 -0500

    ++ center tags and blog content

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { formatDate } from '$lib/utils';
+	import { formatDate } from "$lib/utils";
 	export let data: any;
 </script>
 
@@ -15,46 +15,26 @@
 			<h1>{data.meta.title}</h1>
 			<p>Publicado {formatDate(data.meta.date)}</p>
 		</hgroup>
-	
-		<div class="tags">
+
+		<div class="tags text-center">
 			{#each data.meta.categories as category}
 				<span class="surface-4">&num;{category}</span>
 			{/each}
 		</div>
-
-	</div>
-
-
-	<div class="prose">
-		<svelte:component this={data.content} />
 	</div>
 
 	<div class="prose">
-		<p>This is a paragraph.</p>
-		<div>
-			<p>This is another paragraph.</p>
-		</div>
+		<svelte:component this={data.content} class="container" />
 	</div>
 </article>
 
 <style>
-	.blog-header {
-		max-inline-size: var(--size-content-3);
-		margin-inline: auto;
-	}
-
-	h1 {
-		text-transform: capitalize;
-	}
-
 	h1 + p {
 		margin-top: var(--size-2);
 		color: var(--text-2);
 	}
 
 	.tags {
-		display: flex;
-		gap: var(--size-3);
 		margin-top: var(--size-7);
 	}
 
@@ -62,5 +42,4 @@
 		padding: var(--size-2) var(--size-3);
 		border-radius: var(--radius-round);
 	}
-	
 </style>
