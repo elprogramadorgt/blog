876b57d4f06525b4d8520a543049635146baf503
Edu <elprogramadorgt8@gmail.com>
2023-09-25 21:18:39 -0600

    ++ implement the new getDocumentMetadata function and adapt the new blog file structure

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,21 +1,22 @@
 import { json } from '@sveltejs/kit'
 import type { Post } from '$lib/types'
+import { getDocumentMetadata } from '$lib/utils/document'
 
 
 async function getPosts() {
 
     let posts: Post[] = []
 
-    const paths = import.meta.glob('/src/posts/*.shark', { eager: true })
+    const paths = import.meta.glob('/src/content/posts/**/*.shark', { eager: true })
 
     for (const path in paths) {
         const file = paths[path];
         const slug = path.split('/').at(-1)?.replace('.shark', '')
 
-        if (file && typeof file === 'object' && 'metadata' in file && slug) {
-            const metadata = file.metadata as Omit<Post, 'slug'>
-            const post = { ...metadata, slug } satisfies Post
-            post.published && posts.push(post)
+        const doc = getDocumentMetadata(file, slug)
+
+        if(!!doc){
+            posts.push(doc)
         }
     }
 
