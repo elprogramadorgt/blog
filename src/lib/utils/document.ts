fa505d581035d2e870e037181ef2458dc7ebab73
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-09-28 20:25:07 -0500

    ++ format document util

--- src/lib/utils/document.ts
+++ src/lib/utils/document.ts
@@ -2,10 +2,12 @@ import type { Post } from "$lib/types"
 
 export function getDocumentMetadata(file: any, slug: string | undefined) {
 
+
     if (file && typeof file === 'object' && 'metadata' in file && slug) {
+
         const metadata = file.metadata as Omit<Post, 'slug'>
         const post = { ...metadata, slug } satisfies Post
-        if(post.published){
+        if (post.published) {
             return post
         }
     }
