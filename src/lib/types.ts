5a0d8887b5d025952f0c84fe41217061fb30005c
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-09-28 23:15:19 -0500

    ++ url to the structure

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,6 +3,7 @@ export type Categories = 'sveltekit' | 'svelte'
 export type Post = {
     title: string
     slug: string
+    url: string
     description: string
     date: string
     categories: Categories[]
