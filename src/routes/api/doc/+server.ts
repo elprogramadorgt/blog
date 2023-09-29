2701f17d70ccc69cd8c0f478b8a8256e01f5943e
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-09-28 20:27:07 -0500

    ++ getDocs Get Endpoint

---
+++ src/routes/api/doc/+server.ts
@@ -0,0 +1,34 @@
+import { json } from '@sveltejs/kit'
+import type { Post } from '$lib/types'
+import { getDocumentMetadata } from '$lib/utils/document'
+
+
+async function getDocs() {
+
+    let posts: Post[] = []
+
+    const paths = import.meta.glob('/src/content/**/*.shark', { eager: true })
+
+    for (const path in paths) {
+        const file = paths[path];
+        const slug = path.split('/').at(-1)?.replace('.shark', '')
+
+        const doc = getDocumentMetadata(file, slug)
+
+        if (!!doc) {
+            posts.push(doc)
+        }
+    }
+
+    posts = posts.sort((first: Post, second: Post) => new Date(second.date).getTime() - new Date(first.date).getTime())
+
+    return posts
+}
+
+
+export async function GET() {
+    const posts = await getDocs()
+
+    return json(posts)
+}
+
