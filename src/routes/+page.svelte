6352f6a51fd0969756290981957e8719ca225c61
Eduardo Alvarez <ealvarez0896@gmail.com>
2024-04-08 11:17:23 -0600

    ++ update home page style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,14 @@
   import ListPosts from "../components/ListPosts.svelte";
 
   export let data;
+
+
+
+  import Navbar from '../components/Navbar.svelte';
+  import Footer from '../components/Footer.svelte';
+
+
+  
 </script>
 
 <svelte:head>
@@ -11,6 +19,24 @@
 </svelte:head>
 
 <!-- Posts -->
-<section>
+
+<!-- <section>
   <ListPosts posts={data.posts} />
-</section>
+</section> -->
+
+
+<Navbar />
+<div class="container mx-auto px-4 py-8">
+  <h1 class="font-heading text-5xl text-center mb-8">Explore Programming & Cybersecurity</h1>
+  
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    {#each data.posts as post}
+    <div class="card p-6 rounded-lg shadow-md">
+      <h2 class="card-title font-heading text-xl font-semibold mb-4">{post.title}</h2>
+        <p class="font-sans text-base mb-4">{post.description}</p>
+        <a href={`/${post.url}/${post.slug}`} class="card-link">Read More</a>
+      </div>
+    {/each}
+  </div>
+</div>
+<Footer />
