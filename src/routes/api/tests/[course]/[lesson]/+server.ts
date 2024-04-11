19e10dab8c678271354eba20018372ac3a6cf98f
Edu <elprogramadorgt8@gmail.com>
2024-04-11 16:18:26 -0600

    ++ get questions and answers

--- src/routes/api/tests/[course]/[lesson]/+server.ts
+++ src/routes/api/tests/[course]/[lesson]/+server.ts
@@ -1,23 +1,68 @@
-import { json } from "@sveltejs/kit";
+import { error, json } from "@sveltejs/kit";
+import fs from "fs/promises";
+import path from "path";
 
-async function getQuestions(course: string, lesson: string) {
-  console.log(course, lesson, " return");
-
+async function getTest(course: string, lesson: string) {
   switch (course.toLowerCase()) {
     case "go":
-      let a = import.meta.glob("/src/content/tests/go/test-1.json", {
-        eager: true,
-      });
-      console.log(a);
+      const filePath = path.resolve(
+        "src/content/tests/go",
+        `test-${lesson}.json`
+      );
 
-      break;
+      const data = await fs.readFile(filePath, "utf-8");
+
+      return json(JSON.parse(data));
   }
-
-  return { ok: true };
 }
 
 export async function GET({ params }: any) {
-  console.log(params, "apaaaraams");
-  const courses = await getQuestions(params.course, params.lesson);
-  return json(courses);
+  console.log(params.course, params.lesson);
+  // The `params.quizId` holds the dynamic part of the URL, e.g., "test-1" for "/quiz/test-1"
+  const { course, lesson } = params;
+
+  try {
+    // Construct the file path to the quiz JSON file
+
+    if (isNaN(Number(lesson))) {
+      return json({ ok: false, error_message: "Invalid Quiz!" });
+    }
+
+    return getTest(course, lesson);
+    // const filePath = path.resolve("src/content/tests/go", `test-1.json`);
+
+    // // Read the file's content
+    // const data = await fs.readFile(filePath, "utf-8");
+
+    // // Return the file content as JSON
+    // return json(JSON.parse(data));
+  } catch (err) {
+    // Handle errors, such as the file not existing
+    console.error(err);
+    return error(404, `Quiz not found: ${quizId}`);
+  }
 }
+
+// import { json } from "@sveltejs/kit";
+
+// async function getQuestions(course: string, lesson: string) {
+//   console.log(course, lesson, " return");
+
+//   switch (course.toLowerCase()) {
+//     case "go":
+//       let a = import.meta.glob("/src/content/tests/go/test-1.json", {
+//         eager: true,
+//       });
+//       console.log(a);
+
+//       break;
+//   }
+
+//   return { ok: true };
+// }
+
+// export async function GET({ params }: any) {
+//   console.log(params, "apaaaraams");
+//   const courses = await getQuestions(params.course, params.lesson);
+//   return json(courses);
+// }
