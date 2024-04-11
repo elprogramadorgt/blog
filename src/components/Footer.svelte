a2187d54d8af647553b6a0fb49ec738fab3e3843
Eduardo Alvarez <ealvarez0896@gmail.com>
2024-04-10 21:48:58 -0600

    ++ Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,4 @@
+<footer class="bg-background-light dark:bg-background-dark text-center p-4 mt-8">
+    <p class="text-text-primary-light dark:text-text-primary-dark">&copy; 2024 ElProgramdorGt Blog</p>
+  </footer>
+  
