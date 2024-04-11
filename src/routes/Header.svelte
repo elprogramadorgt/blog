e3af6d4bf4ea16dec8439c865f627ad0be27c4b6
Eduardo Alvarez <ealvarez0896@gmail.com>
2024-04-10 21:50:00 -0600

    ++ update header code

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,56 +1,7 @@
 <script lang="ts">
 	import * as config from "$lib/config";
+	import Navbar from '../components/Navbar.svelte';
+
 </script>
 
-<nav>
-	<!-- Title -->
-	<a href="/" class="title">
-		<b>{config.title}</b>
-	</a>
-
-	<!-- Navigation -->
-	<ul class="links">
-		<li>
-			<a href="/blog">Blog</a>
-		</li>
-
-		<li>
-			<a href="/courses">Cursos</a>
-		</li>
-
-		<li>
-			<a href="/about">About</a>
-		</li>
-	</ul>
-
-	<!-- Theme -->
-	<button>Toggle</button>
-</nav>
-
-<style>
-	nav {
-		padding-block: var(--size-7);
-	}
-
-	.links {
-		margin-block: var(--size-7);
-	}
-
-	a {
-		color: inherit;
-		text-decoration: none;
-	}
-
-	@media (min-width: 768px) {
-		nav {
-			display: flex;
-			justify-content: space-between;
-		}
-
-		.links {
-			display: flex;
-			gap: var(--size-7);
-			margin-block: 0;
-		}
-	}
-</style>
+<Navbar />
