e68d240cd79b4e6b27faa687590a4ad55472ee04
Eduardo Alvarez <ealvarez0896@gmail.com>
2024-04-10 21:50:21 -0600

    ++ update POST

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,11 @@
-export type Categories = 'sveltekit' | 'svelte'
+export type Categories = "sveltekit" | "svelte";
 
 export type Post = {
-    title: string
-    slug: string
-    url: string
-    description: string
-    date: string
-    categories: Categories[]
-    published: boolean
-}
+  title: string;
+  slug: string;
+  url: string;
+  description: string;
+  date: string;
+  tags: [];
+  published: boolean;
+};
