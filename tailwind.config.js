b60cb5186d02cde9327edcc6656b80fcb2ee635c
Eduardo Alvarez <ealvarez0896@gmail.com>
2024-04-10 21:51:56 -0600

    ++ style config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    // extend: {
+    //   colors: {
+    //     background: {
+    //       light: "#FFFFFF",
+    //       dark: "#111827",
+    //     },
+    //     text: {
+    //       primary: "#111827",
+    //       light: "#F9FAFB",
+    //     },
+    //     accent: "#3B82F6",
+    //     secondary: "#10B981",
+    //     border: {
+    //       light: "#E5E7EB",
+    //       dark: "#374151",
+    //     },
+    //   },
+    //   fontFamily: {
+    //     sans: ["Inter", "sans-serif"],
+    //     heading: ["Montserrat", "sans-serif"],
+    //   },
+    // },
   },
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       primary: "#00D1FF",
+  //       secondary: "#FF007B",
+  //       dark: "#0A0A0A",
+  //       light: "#FFFFFF",
+  //       grey: "#BBBBBB",
+  //     },
+  //     fontFamily: {
+  //       sans: ["Orbitron", "sans-serif"],
+  //       // Add other font families as needed
+  //     },
+  //   },
+  // },
   plugins: [],
-}
-
+};
