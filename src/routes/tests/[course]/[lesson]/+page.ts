befc25b642a5fe23b98265973ce5c14d83319f74
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2024-04-16 22:50:15 -0500

    -- remove unused code

--- src/routes/tests/[course]/[lesson]/+page.ts
+++ src/routes/tests/[course]/[lesson]/+page.ts
@@ -1,13 +1,16 @@
 export async function load({ fetch, params }: any) {
-  try {
-    const response = await fetch(
-      `/api/tests/${params.course}/${params.lesson}`
-    );
-    const data = await response.json();
+  // try {
+  //   console.log("aaaaaa")
+  //   const response = await fetch(
+  //     `/api/tests/${params.course}/${params.lesson}`
+  //   );
 
-    return data;
-  } catch (e) {
-    throw error(404, `No se pudo encontrar el test`);
-  }
+  //   console.log("responseeee",response)
+  //   const data = await response.json();
+
+  //   return data;
+  // } catch (e) {
+  //   throw error(404, `No se pudo encontrar el test`);
+  // }
   // const posts: Post[] = await response.json();
 }
